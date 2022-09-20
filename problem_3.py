66edcb941e489cf2564f8a6046c7df595e63c25a
francisco <franciscoruizvillar@gmail.com>
2022-09-20 11:16:09 +0200

    p3 now managing test 1

--- problem_3.py
+++ problem_3.py
@@ -1,40 +1,43 @@
 class Seed:
     N = 0
 
-    def __init__(self, q, mature_l, v):
-        self.n = self.N + 1
+    def __init__(self, d, q, mature_l, v):
         self.q = q
         self.mature_l = mature_l
         self.v = v
-
-        Seed.N = self.n
-
-    def __repr__(self):
-        return f'{self.n}: q={self.q}, ml={self.mature_l}, v={self.v}'
+        self.deadline = d - mature_l
 
 
 def test():
     # Read integers N and M from the standard input.
     (d, n, x) = tuple(map(int, input().split()))
 
-    seeds = [Seed(*map(int, input().split())) for _ in range(n)]
-    s_seeds = sorted(seeds, key=lambda s: (s.v, -s.mature_l), reverse=True)
+    seeds = [Seed(d, *map(int, input().split())) for _ in range(n)]
+    s_seeds = sorted(seeds, key=lambda s: s.v, reverse=True)
 
     harvest = 0
-    for day in range(1, d + 1):
+    for day in range(d, 0, -1):
         x_day = x
-
-        non_dying_seeds_avail = [s for s in s_seeds if (day + s.mature_l <= d) and s.q]
-
-        non_dying_seeds_avail_sorted_expiry = sorted(non_dying_seeds_avail, key=lambda s: d - day - s.mature_l != 0)
-        for s in non_dying_seeds_avail_sorted_expiry:
+        non_dying_seeds_avail = [s for s in s_seeds if (day <= s.deadline) and s.q]
+        if not non_dying_seeds_avail:
+            #print(f'Day {day}: Cannot plant anything')
+            continue
+        for s in non_dying_seeds_avail:
             n_s = min(s.q, x_day)
             s.q -= n_s
             x_day -= n_s
-            # print(f'{day=}. Planted {n_s} of {s.n=} {s.v=} ({s.mature_l=}). Value={n_s*s.v=}')
+
             harvest += n_s * s.v
+
+            if not s.q:
+                s_seeds.remove(s)
+
             if not x_day:
                 break
+
+        if not any(s.q for s in s_seeds):
+            #print('No more seeds')
+            pass#break
 
     print(harvest)
 
